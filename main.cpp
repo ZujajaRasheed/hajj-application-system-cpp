0033843248f04bec8650b6c4e28584cd034d967e
Zujaja Rasheed <zujajarasheed1912@gmail.com>
2025-05-23 20:27:44 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include"Hajj.h"
+using namespace std;
+
+
+
+int main()
+{
+
+	// Allocate memory for an array of 5 Confirmation objects
+	Confirmation* confirmations = new Confirmation[5];
+
+	// Call the menu function for each Confirmation object
+	for (int i = 0; i < 5; i++) {
+		confirmations[i].menu();
+	}
+
+	//  free the allocated memory
+	delete[] confirmations;
+
+
+
+ return 0;
+}
